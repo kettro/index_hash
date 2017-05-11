c60029617b37e4b414867db4c4af8989c1760c97
kettro <rkettleson@gmail.com>
2017-05-10 19:15:51 -0700

    modification to the test procedure; getting it going

--- src/main.c
+++ src/main.c
@@ -2,21 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdint.h>
 
-
-// Constants
-#define NUL             '\0'
-// Local Variables
-// Local Function Declarations
-// External Variables
-// External Function Declarations
-extern void test_suite(void);
-// Definitions
+extern void testSuite(void);
 
 int main(void)
 {
-  test_suite();
-  return 0;
+    testSuite();
+    return 0;
 }
--- test/test_suite.c
+++ test/test_suite.c
@@ -1,16 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
-#include <unistd.h>
+#include <map.h>
 
-#include "hash_data_structures.h"
-
-void test_suite(void);
-
-void test_suite(void)
+extern void testSuite(void)
 {
-  const char symbol_key[] = "name";
-  uint16_t value = 1002;
+    Map* new_map = newMap();
 
 }
